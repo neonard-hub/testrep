8917e553027e7dea2e97ed4214ed7c9b6ea9044c
neonard <86165920+neonard-hub@users.noreply.github.com>
2021-06-29 10:39:32 -0600

    Add files via upload

---
+++ addmultiplication.py
@@ -0,0 +1,12 @@
+x = 10
+def sum(a, b):
+    print("Sum of two values: " , (a+b))
+
+def multiplication(a, b):
+    print("Multiplication of two values: " , (a*b))
+
+a_list = []
+for number in range(1, 6):
+    a_list += [number]
+
+print(a_list)
